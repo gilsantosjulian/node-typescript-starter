231bd20cafb56148d257044d74038d504dc1fcbe
Julian <julian@minka.io>
2019-06-26 16:38:02 -0500

    fix(api-folder): run prettier and tslint

--- vendors/api/db/datastore/users/get/getMany.ts
+++ vendors/api/db/datastore/users/get/getMany.ts
@@ -1,8 +1,8 @@
-import http from "../../../../services/http";
+import http from '../../../../services/http';
 
 const getMany = async (filters: object): Promise<any> => {
   if (filters) {
-    const response = await http.get("https://pokeapi.co/api/v2/pokemon");
+    const response = await http.get('https://pokeapi.co/api/v2/pokemon');
     return response.data.results;
   }
   new Error('Filter object required');
