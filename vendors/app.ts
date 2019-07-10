090a3331877bb22d83e715b7a0299469b74c719d
Julian <julian@minka.io>
2019-07-09 23:17:46 -0500

    ci(app.ts): debug

--- vendors/app.ts
+++ vendors/app.ts
@@ -10,20 +10,27 @@ const PORT = config.PORT;
 
 async function startServer() {
   const app = express();
-
+  console.log('step 1');
   app.get('/', routes.rootRoute);
+  console.log('step 2');
   app.use('/users', routes.usersRoute);
+  console.log('step 3');
   app.use('/vendors', routes.vendorsRoute);
+  console.log('step 4');
 
   app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     if (error) {
+      console.log('step 5');
       logging.error(error);
     }
+    console.log('step 6');
     res.status(500).send(error.message);
   });
+  console.log('step 7');
 
   app.listen(PORT, (err: any) => {
     if (err) {
+      console.log('step 8');
       logging.error(err);
       return;
     }
