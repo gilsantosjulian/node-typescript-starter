5c57d366636671d6d11168a4f5e9ed984209aab7
Luis E. Alfonso <luealfonsoru@unal.edu.co>
2019-07-15 18:18:00 -0500

    feat(querybuilder.ts): gt and lt added

--- vendors/api/db/cloud-sql/read/helpers/queryBuilder.ts
+++ vendors/api/db/cloud-sql/read/helpers/queryBuilder.ts
@@ -1,28 +1,35 @@
-const addWhereValue = (filterString: string, element: string, operator: string) => {
-  const queryElement = `${element} = :${element}`;
-  if (filterString.length === 0) {
-    return (filterString = queryElement); // TODO validate element on queries get request
+const addOperator = (buildedQuery: any, key: string, operator: string, value: any) => {
+  const operatorParser: any = {
+    eq: '=',
+    lt: '<',
+    gt: '>',
+  };
+  const queryVariables: any = {};
+
+  if (operator != 'between') {
+    queryVariables[key] = value;
+    return buildedQuery.andWhere(
+      `${key} ${operatorParser[operator]} :${key}`,
+      queryVariables,
+    );
   } else {
-    return (filterString = ` ${operator} ${queryElement}`);
+    queryVariables[`${key}1`] = value.lt;
+    queryVariables[`${key}2`] = value.gt;
+    return buildedQuery.andWhere(
+      `${key} < :${key}1 AND ${key} > :${key}2`,
+      queryVariables,
+    );
   }
 };
 
 const addWhere = (filter: any, buildedQuery: any) => {
-  let filterString: string = '';
-  const operator: string = 'AND';
-  const filterObject: any = {};
   Object.keys(filter).forEach(key => {
     if (filter[key].eq) {
-      filterString = filterString + addWhereValue(filterString, key, operator);
-      filterObject[key] = filter[key].eq;
+      buildedQuery = addOperator(buildedQuery, key, 'eq', filter[key].eq);
     }
   });
 
-  if (filterString.length > 0) {
-    return buildedQuery.where(filterString, filterObject);
-  } else {
-    return buildedQuery;
-  }
+  return buildedQuery;
 };
 
 const addPaginator = (filter: any, buildedQuery: any) => {
@@ -30,21 +37,33 @@ const addPaginator = (filter: any, buildedQuery: any) => {
 };
 
 const addOrderBy = (filter: any, buildedQuery: any) => {
-  console.log(filter);
+  Object.keys(filter).forEach(key => {
+    if (filter[key].lt && filter[key].gt) {
+      buildedQuery = addOperator(buildedQuery, key, 'between', {
+        lt: filter[key].lt,
+        gt: filter[key].gt,
+      });
+    } else if (filter[key].lt) {
+      buildedQuery = addOperator(buildedQuery, key, 'lt', filter[key].lt);
+    } else if (filter[key].gt) {
+      buildedQuery = addOperator(buildedQuery, key, 'gt', filter[key].gt);
+    }
+  });
+  return buildedQuery;
 };
 
 const builder = async (filter: any, buildedQuery: any) => {
-  const whereFilter = filter;
-  delete whereFilter.page;
-  delete whereFilter.pageSize;
+  const noPagingFilter = filter;
+  delete noPagingFilter.page;
+  delete noPagingFilter.pageSize;
 
-  buildedQuery = addWhere(whereFilter, buildedQuery);
+  buildedQuery = addWhere(noPagingFilter, buildedQuery);
+
+  buildedQuery = addOrderBy(filter, buildedQuery);
 
   if (filter.page && filter.pageSize) {
     buildedQuery = addPaginator(filter, buildedQuery);
   }
-
-  addOrderBy(filter, buildedQuery);
 
   return buildedQuery.getMany();
 };
