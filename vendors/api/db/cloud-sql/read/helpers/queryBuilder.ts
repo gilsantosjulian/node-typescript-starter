140c3a18e60fa929e72af59a96bb5a98b2416dcb
Luis E. Alfonso <luealfonsoru@unal.edu.co>
2019-07-17 19:24:10 -0500

    feat(querybuilder.ts): added groupby filter

--- vendors/api/db/cloud-sql/read/helpers/queryBuilder.ts
+++ vendors/api/db/cloud-sql/read/helpers/queryBuilder.ts
@@ -1,13 +1,41 @@
+const splitParamsByComa = (filter: any) => {
+  console.log(filter);
+  const filterParams: String[] = filter.split(',');
+  filterParams.forEach((item, index) => {
+    filterParams[index] = `entity.${item}`;
+  });
+  return filterParams;
+};
+
+const copyJSON = (json: any) => {
+  const newJson: any = {};
+  Object.keys(json).forEach(key => {
+    newJson[key] = json[key];
+  });
+  return newJson;
+};
+
+const sanitizeFilter = (filter: any) => {
+  delete filter.select;
+  delete filter.page;
+  delete filter.groupBy;
+  delete filter.pageSize;
+};
+
+// TODO validations and helpers has to be in controller, utils or helper: they are here in order to make filter works
+
 const addOperator = (buildedQuery: any, key: string, operator: string, value: any) => {
   const operatorParser: any = {
     eq: '=',
     lt: '<',
     gt: '>',
+    gte: '>=',
+    lte: '<=',
   };
   const queryVariables: any = {};
-
   if (operator != 'between') {
     queryVariables[key] = value;
+    console.log(`${key} ${operatorParser[operator]} :${key}`);
     return buildedQuery.andWhere(
       `${key} ${operatorParser[operator]} :${key}`,
       queryVariables,
@@ -24,11 +52,10 @@ const addOperator = (buildedQuery: any, key: string, operator: string, value: an
 
 const addWhere = (filter: any, buildedQuery: any) => {
   Object.keys(filter).forEach(key => {
-    if (filter[key].eq) {
-      buildedQuery = addOperator(buildedQuery, key, 'eq', filter[key].eq);
+    if (!filter[key].lt && !filter[key].gt && !filter[key].sort) {
+      buildedQuery = addOperator(buildedQuery, key, 'eq', filter[key]);
     }
   });
-
   return buildedQuery;
 };
 
@@ -53,24 +80,49 @@ const addInRange = (filter: any, buildedQuery: any) => {
 };
 
 const addSelect = (filter: any, buildedQuery: any) => {
-  const filterParams: String[] = filter.select.split(',');
-
-  filterParams.forEach((item, index) => {
-    filterParams[index] = `entity.${item}`;
-  });
+  console.log(filter.select, 'the filter');
+  const filterParams: String[] = splitParamsByComa(filter.select);
+  console.log(filterParams);
 
   buildedQuery.select(filterParams);
 
   return buildedQuery;
 };
 
+const addOrderBy = (filter: any, buildedQuery: any) => {
+  Object.keys(filter).forEach(key => {
+    if (filter[key].sort) {
+      const sortDirection = filter[key].sort;
+      buildedQuery.addOrderBy(`entity.${key}`, sortDirection);
+    }
+  });
+  return buildedQuery;
+};
+
+const addGroupBy = (filter: any, buildedQuery: any) => {
+  const filterParams: String[] = splitParamsByComa(filter.groupBy);
+  filterParams.forEach(element => {
+    buildedQuery.addGroupBy(element);
+  });
+  buildedQuery.addGroupBy('id');
+
+  return buildedQuery;
+};
+
 const builder = async (filter: any, buildedQuery: any) => {
+  const sanitizedFilter: any = copyJSON(filter);
+  sanitizeFilter(sanitizedFilter);
+
+  console.log(sanitizedFilter);
   if (filter.select) {
     buildedQuery = addSelect(filter, buildedQuery);
   }
-
-  buildedQuery = addWhere(filter, buildedQuery);
+  buildedQuery = addWhere(sanitizedFilter, buildedQuery);
   buildedQuery = addInRange(filter, buildedQuery);
+  buildedQuery = addOrderBy(filter, buildedQuery);
+  if (filter.groupBy) {
+    buildedQuery = addGroupBy(filter, buildedQuery);
+  }
   buildedQuery = addPaginator(filter, buildedQuery);
   return await buildedQuery.getManyAndCount();
 };
